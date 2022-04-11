ffa4286dc0b787f95673ed89958825d29fb3bd4c
FLorian Stoegmueller <florian@tischlerei-krieg.com>
2022-04-11 22:58:02 +0200

    testing file IO

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include <fstream>
 
 #include "../include/base64.h"
 
@@ -15,16 +16,52 @@ extern "C" {
 }
 #endif
 
+std::string encode(uint8_t* in, int size){
+    std::string out;
+    for (int i = 0; i < size; i++) {
+        out = out + (char)in[i];
+    }
+    return base64_encode(out);
+}
+
+void decode(std::string in, uint8_t* out, int size){
+    in = base64_decode(in);
+    for (int i = 0; i < size; i++) {
+        out[i] = in[i];
+    }
+}
+
 int main(void) {
     uint8_t pk[CRYPTO_PUBLICKEYBYTES];
     uint8_t sk[CRYPTO_SECRETKEYBYTES];
     uint8_t ct[CRYPTO_CIPHERTEXTBYTES];
     uint8_t key_a[CRYPTO_BYTES];
     uint8_t key_b[CRYPTO_BYTES];
+
+    std::ofstream keyfile;
+    std::ifstream keyfile2;
+    std::string buf;
+    uint8_t sk2[CRYPTO_SECRETKEYBYTES];
     
+    // generate keypair
     crypto_kem_keypair(pk, sk);
+
+    // write secrete key to file
+    keyfile.open("key.txt");
+    keyfile << encode(sk, CRYPTO_SECRETKEYBYTES) << "\n";
+    keyfile.close();
+
+    // encrypt
     crypto_kem_enc(ct, key_b, pk);
-    crypto_kem_dec(key_a, ct, sk);
+
+    // retrive key from file
+    keyfile2.open("key.txt");
+    std::getline(keyfile2, buf);
+    keyfile2.close();
+    decode(buf, sk2, CRYPTO_SECRETKEYBYTES);
+
+    // decrypt with key from file
+    crypto_kem_dec(key_a, ct, sk2);
 
     if (memcmp(key_a, key_b, CRYPTO_BYTES)) {
         printf("ERROR keys\n");
@@ -32,10 +69,4 @@ int main(void) {
     } else {
         printf("success\n");
     }
-
-    std::string encode = base64_encode("Test");
-    std::cout << "\nencode:\n" << encode << "\n";
-    std::string decode = base64_decode(encode);
-    std::cout << "\ndecode:\n" << decode << "\n";
-
 }
